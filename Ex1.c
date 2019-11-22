a86a6ba9e33565e3e7be3508ee564c827fc5cc43
Leo-chen570 <493075073@qq.com>
2019-11-22 00:05:20 -0800

    added a Ex1.c file

--- Ex1.c
+++ Ex1.c
@@ -1,27 +1,17 @@
-#include <stdio.h>
-int main()
-{
-char  c;
-int letters=0,space=0,digit=0,'\t'=0;
-printf("请输入一行字符：");
-while ((c=getchar())!='\n')
-{
-if (c >= 'a'&&c <= 'z' || c >= 'A'&&c <= 'Z')
-{
-letters++;
-}
-else if (c == ' ')
-{
-space++;
-}
-else if (c >= '0'&&c <= '50')
-{
-digit++;
-}
-else
-{
-'\t'++;
-}
-}
-printf("单词的个数%d\n,word");
-return 0;
+#include<stdio.h>
+int main(){
+    printf("输入一段字符(总长度不超过50个)\n");
+    char x[50];
+    int i = 0;
+    int amount = 0;
+    int k = 0;
+    while((x[i]= getchar()) != '\n'){
+        i++;
+    }
+    for(k = 0;k<i;k++){
+        if(x[k]>='A'&&x[k]<='Z');
+    }
+    printf("单词的个数: %d\n",amount);
+    return 0;
+} 
+
